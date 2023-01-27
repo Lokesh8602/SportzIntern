2106103eca8a0f05cd849aa7d42bf6b2287919a5
Lokesh8602 <lokeshpatil8602@gmail.com>
2023-01-27 11:19:26 +0530

    Add Alert dialog Box

--- app/src/main/java/com/example/sportzinter/adapter/DetailsAdapter.kt
+++ app/src/main/java/com/example/sportzinter/adapter/DetailsAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.sportzinter.adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sportzinter.R
 import com.example.sportzinter.databinding.DetailsItemBinding
 import com.example.sportzinter.databinding.MatchDataBinding
 import com.example.sportzinter.databinding.PlayerListBinding
@@ -34,7 +36,42 @@ class DetailsAdapter(val context: Context, val playerDetailData: MutableList<Pla
         val playerData = playerDetailData[position]
         holder.apply {
             binding.playerName.text = playerData.NameFull
+
+            if (playerData.isKeeper == true && playerData.isCaptain == true) {
+                binding.playerName.text = playerData.NameFull + "(" + "C/WK" + ")"
+            } else if (playerData.isCaptain == true) {
+                binding.playerName.text = playerData.NameFull + "(" + "C" + ")"
+            } else if (playerData.isKeeper == true) {
+                binding.playerName.text = playerData.NameFull + "(" + "WKq" + ")"
+            } else {
+
+                binding.playerName.text = playerData.NameFull
+
+            }
+            binding.playerName.setOnClickListener {
+                val builder = AlertDialog.Builder(context, com.google.android.material.R.style.Base_ThemeOverlay_AppCompat_Dialog).create()
+                val view = LayoutInflater.from(context).inflate(R.layout.alert_dialog, null)
+                builder.setView(view)
+                val customTextView = view.findViewById<TextView>(R.id.tvBatting)
+                customTextView.text =  "Name: " + playerData.NameFull
+                val customTextView1 = view.findViewById<TextView>(R.id.tvStrikeRate)
+                customTextView1.text =  "Strike rate is : " + playerData.Batting?.strikeRate
+                val customTextView2 = view.findViewById<TextView>(R.id.tvAverage)
+                customTextView2.text = "Average is : "+ playerData.Batting?.average
+                val customTextView3 = view.findViewById<TextView>(R.id.tvRuns)
+                customTextView3.text = "Total Run is : "+ playerData.Batting?.runs
+                val customTextView4 = view.findViewById<TextView>(R.id.tvStyle)
+                customTextView4.text = "Batting Style : " + playerData.Batting?.style
+
+                customTextView.setOnClickListener {
+                    builder.dismiss()
+                }
+                builder.setCanceledOnTouchOutside(true)
+                builder.show()
+            }
+
         }
+
     }
 
     override fun getItemCount() = playerDetailData.size
--- app/src/main/java/com/example/sportzinter/pojo/Players.kt
+++ app/src/main/java/com/example/sportzinter/pojo/Players.kt
@@ -10,7 +10,27 @@ data class PlayerDetail(
 
     @SerializedName("Position") var Position: String? = null,
     @SerializedName("Name_Full") var NameFull: String? = null,
-    /* @SerializedName("Batting"   ) var Batting  : Batting? = Batting(),
-     @SerializedName("Bowling"   ) var Bowling  : Bowling? = Bowling()
-*/
+     @SerializedName("Batting"   ) var Batting  : Batting? = Batting(),
+     @SerializedName("Bowling"   ) var Bowling  : Bowling? = Bowling(),
+    @SerializedName("Iskeeper") var isKeeper: Boolean? = null,
+    @SerializedName("Iscaptain") var isCaptain: Boolean? = null,
+
+) : java.io.Serializable
+
+data class Batting(
+
+    @SerializedName("Style") var style: String? = null,
+    @SerializedName("Average") var average: String? = null,
+    @SerializedName("Strikerate") var strikeRate: String? = null,
+    @SerializedName("Runs") var runs: String? = null
+
+) : java.io.Serializable
+
+data class Bowling(
+
+    @SerializedName("Style") var style: String? = null,
+    @SerializedName("Average") var average: String? = null,
+    @SerializedName("Economyrate") var economyRate: String? = null,
+    @SerializedName("Wickets") var wickets: String? = null
+
 ) : java.io.Serializable
